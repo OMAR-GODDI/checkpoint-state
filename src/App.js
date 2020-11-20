bae0b07e1e9c348319d8d77a4bc92899843899b7
OMAR-GODDI <73104649+OMAR-GODDI@users.noreply.github.com>
2020-11-20 11:15:04 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+  state = {
+  
+          imgSrc: "https://image.freepik.com/free-vector/man-profile-cartoon_18591-58482.jpg",
+            name: "Omar Goddi",
+            profession: "web developer",
+            age: 27,
+            email: "gd.omar@hotmail.com",
+            address:"Nabeul",
+            isLoading:true,
+            otherLoding: true
+            
+          
+  };
+  
+handleShow =()=>this.setState({isShow :!this.state.isShow});
+componentDidMount(){
+  setInterval(()=>this.setState({isLoading: false }), 2500);
+  
+  
+}
+
+
+
+  render() {
+    if (this.state.isLoading) {
+      return <h1>loading......</h1>
+     {
+     
+        
+      }
+      
+    }
+    
+    return (
+      <div>
+        <h1 className="my-profile" > My profile</h1>
+    <button class="btn btn-primary btn-lg" onClick={()=>this.handleShow()} > {this.state.isShow ? "Hide" : "Show"}{" "}</button>   
+        {this.state.isShow ? (
+      <div>
+      <img  className="avatar"  src={this.state.imgSrc} width="350" height="350"  />
+      <div className="user-card">
+      <h2> FullName : {this.state.name}</h2>
+      <h2> Profession : {this.state.profession}</h2>
+      <h2> Age : {this.state.age}</h2>
+      <h2> Email: {this.state.email}</h2>
+      <h2> Address : {this.state.address}</h2>  
+      </div>
+      </div>
+        ) : null } 
+          
+      </div>
+    );
+  }
 }
 
 export default App;
